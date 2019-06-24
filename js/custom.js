1ed3a92b9c81352cf9eae99b6c33391724c022ed
noorsagor163 <noor162682@gmail.com>
2019-06-25 00:20:06 +0600

    Slider

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    // ================== Slider Js Start ==================
+
+    $('.recap-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        vertical: true,
+        arrows: false,
+        dots: true,
+    });
+
+    // ================== Slider Js End ==================
+
+    // ================== Venobox Js Start ==================
+
+    $('.venobox').venobox();
+
+    // ================== Venobox Js End ==================
+
+
+
+});
